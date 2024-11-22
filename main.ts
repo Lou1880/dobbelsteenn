161c10f1d43b70eb514b86db4fd7f3027b0b7d98
Lou1880 <louveen987@gmail.com>
2024-11-22 16:03:43 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,14 +1,6 @@
 basic.forever(function () {
-    if (input.compassHeading() > 270 && input.compassHeading() < 360) {
-        basic.showArrow(ArrowNames.East)
-    }
-    if (input.compassHeading() > 0 && input.compassHeading() < 90) {
-        basic.showArrow(ArrowNames.North)
-    }
-    if (input.compassHeading() > 90 && input.compassHeading() < 180) {
-        basic.showArrow(ArrowNames.West)
-    }
-    if (input.compassHeading() > 180 && input.compassHeading() < 270) {
-        basic.showArrow(ArrowNames.South)
-    }
+    led.plotBarGraph(
+    input.soundLevel(),
+    0
+    )
 })
